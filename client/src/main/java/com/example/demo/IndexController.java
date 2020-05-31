235bb904039289f42f8b97ebd13ddd9c16c0115d
vicftz <victorfritz.ici@gmail.com>
2020-05-31 10:50:30 +0200

    add front page

---
+++ client/src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome.html";
+    }
+}
